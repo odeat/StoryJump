6ab4ad1d13f3e955b062766b3e4264ae00c1ed36
odeat <dua2003@live.nl>
2026-04-13 15:25:12 +0200

    push push push

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route index element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
