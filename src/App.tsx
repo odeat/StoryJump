13c54b27a00c55942d9d478589ac05c971f60136
odeat <dua2003@live.nl>
2026-04-13 15:13:16 +0200

    WORK PLS

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/StoryJump">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
